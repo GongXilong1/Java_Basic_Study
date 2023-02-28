23664ea3c30e47ecdcf7fbb2ba7dfec04209abe4
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-02-28 21:29:43 +0800

    Add files via upload

---
+++ basic_code/day02_code/src/Java_basic1/ValueDemo1.java
@@ -0,0 +1,33 @@
+package Java_basic1;
+
+public class ValueDemo1 {
+    public static void main(String[] args){
+        //目标:需要大家掌握常见的数据在代码中如何书写的?
+
+        //整数:
+        System.out.println(666);
+        System.out.println(-777);
+
+        //小数
+        System.out.println(1.93);
+        System.out.println(-3.71);
+
+        // 字符串--用双引号引起来.
+        System.out.println("哈哈哈");
+        System.out.println("Jason");
+
+        // 字符--用单引号引起来,每个单引号中只有一个字符.
+        System.out.println('男');
+        System.out.println('女');
+
+        // 布尔
+        System.out.println(true);
+        System.out.println(false);
+
+        // 空类型
+        // 细节: null不能直接打印,如果要打印,只能用字符串的形式进行打印.
+        System.out.println("null");
+
+    }
+
+}
