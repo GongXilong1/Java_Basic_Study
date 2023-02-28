1980502fecd718572b0f159579de72007125c2bc
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-02-28 21:37:27 +0800

    Add files via upload

---
+++ basic_code/day02_code/src/Java_basic1/VariableTest.java
@@ -0,0 +1,22 @@
+package Java_basic1;
+
+public class VariableTest {
+    public static void main(String[] args){
+        // 一开始没有乘客
+        int passengers_count = 0;  // 定义变量passengers_count表示乘客数量,并赋值初始值为0表示没有乘客.
+        // 第一站: 上车一位乘客--在原有基础上加1
+        passengers_count = passengers_count + 1;  // 先计算等号后边的,再将计算结果赋值给左边的变量.
+        // 第二站: 上车两位乘客,下车一位乘客
+        passengers_count = passengers_count + 2 - 1;
+        // 第三站: 上车两位乘客,下车一位乘客
+        passengers_count = passengers_count + 2 - 1;
+        // 第四站: 下车一位乘客.
+        passengers_count = passengers_count -1;
+        // 第五站; 上车一位乘客.
+        passengers_count = passengers_count + 1;
+        // 请问: 到终点站,车上还剩几位乘客?
+        System.out.println(passengers_count);
+
+
+    }
+}
