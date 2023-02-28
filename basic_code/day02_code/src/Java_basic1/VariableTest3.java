c49e9c488d708417391bd0a573003d985e8bf3b3
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-01 03:05:31 +0800

    Add files via upload

---
+++ basic_code/day02_code/src/Java_basic1/VariableTest3.java
@@ -0,0 +1,21 @@
+package Java_basic1;
+
+public class VariableTest3 {
+    public static void main(String[] args){
+        // 定义变量记录电影的名字
+        String filmName = "送初恋回家";
+        // 定义变量记录主演的名字
+        String LeadActor_name = "刘鑫 张雨提";
+        // 定义变量记录电影的年份
+        int fileYear = 2020;
+        // 定义变量记录电影的评分
+        double filmRating = 9.0;
+
+        // 输出所有变量的值
+        System.out.println(filmName);
+        System.out.println(LeadActor_name);
+        System.out.println(fileYear);
+        System.out.println(filmRating);
+
+    }
+}
