70da889cf9cb83b7e2c71d1626d71d18de483f49
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-02-28 23:59:10 +0800

    Add files via upload

---
+++ basic_code/day02_code/src/Java_basic1/VariableDemo3.java
@@ -0,0 +1,21 @@
+package Java_basic1;
+
+public class VariableDemo3 {
+    public static void main(String[] args){
+        // byte  取值范围:-128~127
+        byte b1 = 10;
+        System.out.println(b1);
+
+        // short  取值范围:-32768~32767
+        short s1 = 20000;
+        System.out.println(s1);
+
+        // int  取值范围:-2147483648~2147483647
+        int i = 1588888888;
+        System.out.println(i);
+        
+        // long  取值范围:
+
+
+    }
+}
