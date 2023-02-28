5f13c489018f66dd895044a04bcb6d46ff44f5bf
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-01 03:06:19 +0800

    Add files via upload

---
+++ basic_code/day02_code/src/Java_basic1/VariableTest4.java
@@ -0,0 +1,15 @@
+package Java_basic1;
+
+public class VariableTest4 {
+    public static void main(String[] args){
+        // 定义变量记录手机品牌
+        String mobilePhoneName = "华为";
+        // 定义变量记录手机价格
+        double mobilePhonePrise = 5299.0;
+
+        // 输出信息
+        System.out.println(mobilePhoneName);
+        System.out.println(mobilePhonePrise);
+
+    }
+}
