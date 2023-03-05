c2a13d4eeb45b7f7e054b08e5f02ed1f20493fd6
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-05 22:19:07 +0800

    Add files via upload

---
+++ basic_code/day04_code/src/OrderDemo/OrderDemo1.java
@@ -0,0 +1,11 @@
+package OrderDemo;
+/**
+ * 演示 Java中的顺序结构
+ * */
+public class OrderDemo1 {
+    public static void main(String[] args) {
+        System.out.println("努力做主人喜欢的事情");
+        System.out.println("大小姐驾到! 统统闪开!");
+        System.out.println("心怀不惧,方能翱翔于天际");
+    }
+}
