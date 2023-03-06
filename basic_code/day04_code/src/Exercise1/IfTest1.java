8c1bbd810a2353bbea7248f563a8e381c24a0c26
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-06 22:15:04 +0800

    Add files via upload

---
+++ basic_code/day04_code/src/Exercise1/IfTest1.java
@@ -0,0 +1,17 @@
+package Exercise1;
+/**
+ * 小红对小明说: 如果你这次考试全班第一,我就做你女朋友.
+ * */
+public class IfTest1 {
+    public static void main(String[] args) {
+        // 1. 定义变量记录小明的名次
+        int ranking = 1;
+        // 2. 对小明的名次进行判断
+        if(ranking == 1){
+            System.out.println("小红成为了小明的女朋友");
+        }
+
+
+
+    }
+}
