ba287243ad9c213aecc6fb68e7f917e9b7399844
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-08 21:14:36 +0800

    Add files via upload

---
+++ basic_code/day04_code/src/Exercise1/ForTest1.java
@@ -0,0 +1,20 @@
+package Exercise1;
+
+/**
+ * 利用 for循环练习:
+ * 打印 1~5
+ * 打印 5~1
+ */
+public class ForTest1 {
+    public static void main(String[] args) {
+        // 打印 1~5
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+
+        // 打印5~1
+        for(int a = 5; a > 0; a--){
+            System.out.println(a);
+        }
+    }
+}
