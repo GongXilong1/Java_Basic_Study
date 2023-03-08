ecdebfdc89759dabc500452fc12925ad87fc1904
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-08 21:13:22 +0800

    Add files via upload

---
+++ basic_code/day04_code/src/LoopDemo/ForDemo1.java
@@ -0,0 +1,13 @@
+package LoopDemo;
+
+/**
+ * 演示 For循环的使用,打印五次 HelloWorld
+ */
+public class ForDemo1 {
+    public static void main(String[] args) {
+        // 1. 变量i, 1~5
+        for (int i = 1; i <= 10 ; i++){
+            System.out.println("HelloWorld");
+        }
+    }
+}
