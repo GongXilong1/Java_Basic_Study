e97620436ae50bdb4b16ee0c9e7b468cd97aa253
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-03-16 23:39:35 +0800

    Add files via upload

---
+++ basic_code/day06_code/src/MethodDemo/MethodDemo2.java
@@ -0,0 +1,18 @@
+package MethodDemo;
+
+/**
+ * 目标: 利用方法最简单的格式完成当前练习
+ */
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        // 调用方法
+        printGFInfo();
+    }
+
+    public static void printGFInfo(){
+        System.out.println("小佳敏");
+        System.out.println("女");
+        System.out.println("18岁");
+        System.out.println("小佳敏");
+    }
+}
