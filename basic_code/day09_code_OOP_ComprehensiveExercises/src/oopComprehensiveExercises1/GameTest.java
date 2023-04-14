56cd33545a3c68692fa601c8c54676375dc69e8f
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-04-14 22:15:37 +0800

    Add files via upload

---
+++ basic_code/day09_code_OOP_ComprehensiveExercises/src/oopComprehensiveExercises1/GameTest.java
@@ -0,0 +1,32 @@
+package oopComprehensiveExercises1;
+
+/**
+ * 文字游戏测试类
+ */
+public class GameTest {
+    public static void main(String[] args) {
+        // 1. 创建第一个角色
+        Role r1 = new Role("乔峰",100);
+        // 2. 创建第二个角色
+        Role r2 = new Role("鸠摩智",100);
+
+        // 3. 开始格斗--回合制
+        while(true){
+            // r1开始攻击r2
+            r1.attack(r2);
+            // 判断r2的剩余血量
+            if (r2.getBloodVolume() == 0){
+                System.out.println(r1.getName() + "K.O了" + r2.getName());
+                break;
+            }
+
+            // r2开始攻击r1
+            r2.attack(r1);
+            // 判断r1剩余血量
+            if (r1.getBloodVolume() == 0){
+                System.out.println(r2.getName() + "K.O了" + r1.getName());
+                break;
+            }
+        }
+    }
+}
